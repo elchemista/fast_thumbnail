98947d6fa3aaaa98c40a16c89e0216e4f29068c5
Yuriy Zhar <darkyfelis@gmail.com>
2025-03-05 18:17:04 +0100

    init elixir project

---
+++ lib/fast_thumbnail.ex
@@ -0,0 +1,18 @@
+defmodule FastThumbnail do
+  @moduledoc """
+  Documentation for `FastThumbnail`.
+  """
+
+  @doc """
+  Hello world.
+
+  ## Examples
+
+      iex> FastThumbnail.hello()
+      :world
+
+  """
+  def hello do
+    :world
+  end
+end
---
+++ test/fast_thumbnail_test.exs
@@ -0,0 +1,8 @@
+defmodule FastThumbnailTest do
+  use ExUnit.Case
+  doctest FastThumbnail
+
+  test "greets the world" do
+    assert FastThumbnail.hello() == :world
+  end
+end
