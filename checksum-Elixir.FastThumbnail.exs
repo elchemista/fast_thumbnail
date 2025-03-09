cca29e16a9a289e0aed298a3eac06ba2a454a1fb
Yuriy Zhar <darkyfelis@gmail.com>
2025-03-09 01:39:40 +0100

    clean up

---
+++ checksum-Elixir.FastThumbnail.exs
@@ -0,0 +1,12 @@
+%{
+  "fast_thumbnail-v0.1.4-nif-2.15-x86_64-pc-windows-gnu.dll.tar.gz" => "sha256:25434f67af6137a0688cda10e35f1c07d691f208158571d3177654de49487798",
+  "fast_thumbnail-v0.1.4-nif-2.15-x86_64-pc-windows-msvc.dll.tar.gz" => "sha256:e20b916ffe0aa05693f76963904f120ba98f6ba6f705f47da138abea4bef05d6",
+  "libfast_thumbnail-v0.1.4-nif-2.15-aarch64-apple-darwin.so.tar.gz" => "sha256:060f7a5f22e334e226cf5386e57c68c4d8a452943c5e45fee55916b347b7014e",
+  "libfast_thumbnail-v0.1.4-nif-2.15-aarch64-unknown-linux-gnu.so.tar.gz" => "sha256:ba63cac71068dc136d1ca04e1d4fcbfa1a88535afa29b5b78119cc822e3d004a",
+  "libfast_thumbnail-v0.1.4-nif-2.15-aarch64-unknown-linux-musl.so.tar.gz" => "sha256:18856377b9402c4fadcd8717493c29234b240e03d87ad8638e4766045aa4f01c",
+  "libfast_thumbnail-v0.1.4-nif-2.15-arm-unknown-linux-gnueabihf.so.tar.gz" => "sha256:7620e8152f0ba3e4eaf4fc65947111defcd15069cfa942c2bf25a2a9519d9e45",
+  "libfast_thumbnail-v0.1.4-nif-2.15-riscv64gc-unknown-linux-gnu.so.tar.gz" => "sha256:954c8e921f7acc4d8d6e4af6f10b6f9a6a7447847c147ba26e257fbe217d5af2",
+  "libfast_thumbnail-v0.1.4-nif-2.15-x86_64-apple-darwin.so.tar.gz" => "sha256:ea96f24ad087c19c96fd7e88f2c484c42df2e7d0eb78b961cc3a9dddd63a326e",
+  "libfast_thumbnail-v0.1.4-nif-2.15-x86_64-unknown-linux-gnu.so.tar.gz" => "sha256:cc5b660cb448d6d4bee428db8195efeb586f2f7ce1f0152f20aa3ce738d5b5d0",
+  "libfast_thumbnail-v0.1.4-nif-2.15-x86_64-unknown-linux-musl.so.tar.gz" => "sha256:e186cbdb01663a585118eab0a21d5419f48bfb07e8642fc5d25338a47c8d4770",
+}
